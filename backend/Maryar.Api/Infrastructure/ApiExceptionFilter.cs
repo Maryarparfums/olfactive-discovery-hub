9a64b34e75a63277ca9b80f7c49cae3a29d45196
Maryarparfums <maryarparfums@gmail.com>
2026-06-09 16:22:49 -0300

    Update ApiExceptionFilter.cs

--- backend/Maryar.Api/Infrastructure/ApiExceptionFilter.cs
+++ backend/Maryar.Api/Infrastructure/ApiExceptionFilter.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Diagnostics;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http.Filters;
 
 namespace Maryar.Api.Infrastructure
@@ -10,23 +7,12 @@ namespace Maryar.Api.Infrastructure
     {
         public override void OnException(HttpActionExecutedContext ctx)
         {
-            Trace.TraceError(ctx.Exception.ToString());
-
-            var status = HttpStatusCode.InternalServerError;
-            string message = "Erro interno do servidor.";
-
-            if (ctx.Exception is UnauthorizedAccessException)
-            {
-                status = HttpStatusCode.Unauthorized;
-                message = "Não autorizado.";
-            }
-            else if (ctx.Exception is ArgumentException)
-            {
-                status = HttpStatusCode.BadRequest;
-                message = ctx.Exception.Message;
-            }
-
-            ctx.Response = ctx.Request.CreateResponse(status, new { error = message });
+            ctx.Response = ctx.Request.CreateResponse(
+                HttpStatusCode.InternalServerError,
+                new
+                {
+                    exception = ctx.Exception.ToString()
+                });
         }
     }
 }
