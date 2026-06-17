e268ee891b07547d06209c0d405ded1053dc373c
Maryarparfums <maryarparfums@gmail.com>
2026-06-16 22:55:58 -0300

    Update AppConfig.cs

--- backend/Maryar.Api/Infrastructure/AppConfig.cs
+++ backend/Maryar.Api/Infrastructure/AppConfig.cs
@@ -4,6 +4,21 @@ namespace Maryar.Api.Infrastructure
 {
     public static class AppConfig
     {
+        public static string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        public static int GetInt(string key)
+        {
+            return int.Parse(ConfigurationManager.AppSettings[key]);
+        }
+
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
         public static string JwtSecret
         {
             get { return ConfigurationManager.AppSettings["JwtSecret"]; }
