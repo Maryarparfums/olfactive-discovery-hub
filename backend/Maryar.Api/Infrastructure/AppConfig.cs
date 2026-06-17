3efcc004aa687e727a59e59bf8fb7422feb80e97
Maryarparfums <maryarparfums@gmail.com>
2026-06-16 23:01:07 -0300

    Update AppConfig.cs

--- backend/Maryar.Api/Infrastructure/AppConfig.cs
+++ backend/Maryar.Api/Infrastructure/AppConfig.cs
@@ -4,6 +4,13 @@ namespace Maryar.Api.Infrastructure
 {
     public static class AppConfig
     {
+        public static int GetInt(string key, int defaultValue = 0)
+        {
+            var val = ConfigurationManager.AppSettings[key];
+            int result;
+            return int.TryParse(val, out result) ? result : defaultValue;
+        }
+        
         public static string Get(string key)
         {
             return ConfigurationManager.AppSettings[key];
