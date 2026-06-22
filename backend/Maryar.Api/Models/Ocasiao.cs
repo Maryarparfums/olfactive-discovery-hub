7498c16e3a666c408fc27cb123f6d3665c7f426d
Maryarparfums <maryarparfums@gmail.com>
2026-06-21 23:09:21 -0300

    Create Ocasiao.cs

---
+++ backend/Maryar.Api/Models/Ocasiao.cs
@@ -0,0 +1,10 @@
+namespace Maryar.Api.Models
+{
+    public class Ocasiao
+    {
+        public string Id          { get; set; }
+        public string Slug        { get; set; }
+        public string Name        { get; set; }
+        public string Description { get; set; }
+    }
+}
